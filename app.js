db3488c59aa5eb781cc9bbc835fb1268fba97d78
cgriffin4 <chris.sgriffin@gmail.com>
2012-03-07 03:36:38 +0000

    "not working"

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.configure('production', function(){
 });
 
 // Routes
-app.get('/', routes.index);
+app.get('/', google.index);
 
 app.listen(process.env.PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
